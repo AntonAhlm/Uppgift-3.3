c3de2272b5f2e43d487c9ee9b5417b1dab6dc37e
Anton <>
2023-02-03 09:13:57 +0100

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vad heter du? : ");
+            string svar = Console.ReadLine();
+            string namn = svar.ToLower(); 
+            
+            if (namn == "stig")
+            {
+                Console.WriteLine("Du har namnsdag idag :D ");
+            } 
+
+            else if (namn == "abraham")
+            {
+                Console.WriteLine("Du har namnsdag imorgon :D");
+            }
+
+            else
+            {
+                Console.WriteLine("Du har inte namnsdag idag eller imorgon :(");
+            }
+
+
+        }
+    }
+}
